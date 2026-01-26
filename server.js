9f50e0985c19afafad9cafd78ac1d32b40b87005
IGGAMING51 <varadkarande0@gmail.com>
2026-01-26 19:46:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+const authRoutes = require("./backend/routes/auth");
+app.use("/api/auth", authRoutes);
+
 
 // Root route (Render health check)
 app.get("/", (req, res) => {
