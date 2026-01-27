6cb84bd55a0c0f0d5b38d4318743a9a6152b5098
IGGAMING51 <varadkarande0@gmail.com>
2026-01-27 21:14:26 +0530

    update public/script.js

---
+++ public/script.js
@@ -0,0 +1,35 @@
+const BASE_URL = https://game-zone-tournaments.onrender.com/;
+
+// SIGNUP
+async function signup() {
+  const gameUid = document.getElementById("su_uid").value;
+  const email = document.getElementById("su_email").value;
+  const password = document.getElementById("su_pass").value;
+
+  const res = await fetch(`${BASE_URL}/api/auth/signup`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ gameUid, email, password })
+  });
+
+  const data = await res.json();
+  document.getElementById("signupMsg").innerText =
+    data.message || data.error;
+}
+
+// LOGIN
+async function login() {
+  const gameUid = document.getElementById("li_uid").value;
+  const email = document.getElementById("li_email").value;
+  const password = document.getElementById("li_pass").value;
+
+  const res = await fetch(`${BASE_URL}/api/auth/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ gameUid, email, password })
+  });
+
+  const data = await res.json();
+  document.getElementById("loginMsg").innerText =
+    data.message || data.error;
+}
