c6c4f3b0faebf9982b17283f47e66498da127343
IGGAMING51 <varadkarande0@gmail.com>
2026-01-27 18:48:24 +0530

    update backend/models/User.js

---
+++ backend/models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+    unique: true,
+    length: 10
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("User", userSchema);
